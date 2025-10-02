1e24ee39a86c54587628a2c2d7f03cfa6d620406
Ghulam Ali <nobbiter580fsd@gmail.com>
2025-10-02 14:06:39 +0500

    Add files via upload

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+
+// Render ya Heroku par PORT environment se milega
+const PORT = process.env.PORT || 3000;
+
+// Static files (HTML, CSS, JS) serve karne ke liye
+app.use(express.static("public"));
+
+// Home Route
+app.get("/", (req, res) => {
+  res.send("Welcome to My Personal Website!");
+});
+
+// Server start karna
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
+
+
